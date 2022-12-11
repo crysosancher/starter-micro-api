8f7e969542a6ac62291dd3a0ce67d173d03a642a
vibhu pandey <77499291+crysosancher@users.noreply.github.com>
2022-12-11 16:27:12 +0000

    Hi

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ http.createServer(function (req, res) {
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+let interval=05*60*1000
+let i=0;
+setInterval(async function(){
+i++
+    console.log("Hemlo checking my self",i)
+    
+}, interval);
