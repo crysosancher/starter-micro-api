110c001de97b661a762eb805d73ed84fcabcb85d
vibhu pandey <77499291+crysosancher@users.noreply.github.com>
2022-12-11 16:29:59 +0000

    he

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
-let interval=05*60*1000
+    let interval=05*60*1000
 let i=0;
 setInterval(async function(){
 i++
     console.log("Hemlo checking my self",i)
     
 }, interval);
+    res.write('Yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
